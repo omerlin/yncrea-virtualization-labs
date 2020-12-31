7dd3afb097d50432de50eac6340bf248738a9a8d
ome13 <omerlin13@gmail.com>
2020-12-31 15:45:16 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,8 +7,6 @@ if [ -e /etc/redhat-release ] ; then
   REDHAT_BASED=true
 fi
 
-TERRAFORM_VERSION="0.12.18"
-PACKER_VERSION="1.2.4"
 # create new ssh key
 [[ ! -f /home/ubuntu/.ssh/mykey ]] \
 && mkdir -p /home/ubuntu/.ssh \
@@ -21,31 +19,40 @@ if [ ${REDHAT_BASED} ] ; then
   yum install -y docker ansible unzip wget
 else 
   apt-get update
-  apt-get -y install docker.io ansible unzip python3-pip
+  #apt-get -y install docker.io ansible unzip python3-pip
+  apt-get -y install docker.io unzip python3-pip
 fi
 # add docker privileges
 usermod -G docker ubuntu
+
 # install awscli and ebcli
-pip3 install -U awscli
-pip3 install -U awsebcli
+# pip3 install -U awscli
+# pip3 install -U awsebcli
 
-#terraform
-T_VERSION=$(/usr/local/bin/terraform -v | head -1 | cut -d ' ' -f 2 | tail -c +2)
-T_RETVAL=${PIPESTATUS[0]}
+TERRAFORM_PACKER_INSTALL=false 
+if [ ${TERRAFORM_PACKER_INSTALL} ] ; then
 
-[[ $T_VERSION != $TERRAFORM_VERSION ]] || [[ $T_RETVAL != 0 ]] \
-&& wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
-&& unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
-&& rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+  TERRAFORM_VERSION="0.12.18"
+  PACKER_VERSION="1.2.4"
 
-# packer
-P_VERSION=$(/usr/local/bin/packer -v)
-P_RETVAL=$?
+  #terraform
+  T_VERSION=$(/usr/local/bin/terraform -v | head -1 | cut -d ' ' -f 2 | tail -c +2)
+  T_RETVAL=${PIPESTATUS[0]}
 
-[[ $P_VERSION != $PACKER_VERSION ]] || [[ $P_RETVAL != 1 ]] \
-&& wget -q https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip \
-&& unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin \
-&& rm packer_${PACKER_VERSION}_linux_amd64.zip
+  [[ $T_VERSION != $TERRAFORM_VERSION ]] || [[ $T_RETVAL != 0 ]] \
+  && wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+  && unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
+  && rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+  # packer
+  P_VERSION=$(/usr/local/bin/packer -v)
+  P_RETVAL=$?
+
+  [[ $P_VERSION != $PACKER_VERSION ]] || [[ $P_RETVAL != 1 ]] \
+  && wget -q https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip \
+  && unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin \
+  && rm packer_${PACKER_VERSION}_linux_amd64.zip
+fi
 
 # clean up
 if [ ! ${REDHAT_BASED} ] ; then
